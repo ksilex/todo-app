aaf07048f00324b5d74dbe6bcaffb2f409a6ebce
ksilex <negative074@gmail.com>
2021-06-03 16:13:51 +0300

    added auth

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,15 +3,25 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	todo "github.com/ksilex/todo-app/models"
 	"github.com/ksilex/todo-app/pkg/repository"
 )
 
-const salt = "asd191klf**s"
+const (
+	salt       = "asd191klf**s"
+	signingKey = "asd13519i(&6a^Sd1jsadssza"
+)
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -21,6 +31,22 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
